bc9f6e3d4ae3e893e47705810b9dc135e47a6113
duaameh <mehdawiduaa@gmail.com>
2024-06-27 21:02:44 +0300

    Add files via upload

---
+++ task1/ConsoleApp1/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Write a C# program that accept two integers and display the smaller. 
+            Console.WriteLine("Enter  two integers number");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x > y) { Console.WriteLine($"the {y} is smaller"); }
+            else { Console.WriteLine($"the {x} is smaller"); }
+
+            //Write a C# conditional statement to find the sign  of input  number
+            Console.WriteLine("enter number ");
+            int x2 = int.Parse(Console.ReadLine());
+            if (x2 < 0)
+            {
+                Console.WriteLine("The sign is -");
+            }
+            else
+            {
+                Console.WriteLine("The sign is +");
+
+            }
+            //sort three numbers ascending
+            Console.WriteLine("Enter three numbers:");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int temp;
+
+            if (num1 > num2)
+            {
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+
+
+
+            if (num2 > num3)
+            {
+                temp = num2;
+                num2 = num3;
+                num3 = temp;
+            }
+            Console.WriteLine($"Sorted numbers: {num1}, {num2}, {num3}");
+            //find the max of five numbers
+            int a = 3;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            int e = 50;
+            if (a > b & a > c & a > d & a > e) { Console.WriteLine(a); }
+            else if (b > a & b > c & b > d & b > e) { Console.WriteLine(b); }
+            else if (c > a & c > b & c > d & c > e) { Console.WriteLine(c); }
+            else if (d > a & d > c & d > b & d > e) { Console.WriteLine(d); }
+            else { Console.WriteLine(e); }
+            // km
+            Console.Write("Enter speed in kilometers per hour (km/h):");
+            double speedKmH = double.Parse(Console.ReadLine());
+
+            double speedMph = speedKmH * 0.621371; ;
+
+            Console.WriteLine($"Speed in miles per hour (mph): {speedMph:F3}");
+            //houres
+            Console.Write("Enter hours:");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Enter minutes:");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.WriteLine($"total is {hours * 60 + minutes}");
+            //minutes
+            Console.Write("Enter minutes :");
+            int minutes2 = int.Parse(Console.ReadLine());
+            int hours2 = 0; int min = minutes;
+            if (minutes / 60 != 0)
+            {
+                hours = minutes / 60;
+                min = minutes % 60;
+            }
+            Console.WriteLine($"The total is{hours} hours , {min}minutes ");
+            //padding
+            string[] sentences = { "Hello",
+                "randooooooom",
+                "Short randooom", "hiiiiiiiii alllllllllllll", "hola" };
+
+            foreach (var sentence in sentences)
+            {
+                Console.WriteLine(sentence.Substring(0, Math.Min(sentence.Length, 9)));
+            }
+
+            Console.WriteLine(ReverseOdd("Bananas"));
+            Console.WriteLine(ReverseOdd("One two three four"));
+        }
+        public static string ReverseOdd(string str)
+        {
+            return string.Join
+                (" ", str.Split(' ').Select(word => word.Length % 2 == 0 ? word : new string(word.Reverse().ToArray())));
+        }
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace task1c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"your name is {name} ");
+            /////// 2-	Define variables with kind of
+            double a = 1.45;
+            string b = "sara";
+            char test = 'A';
+            bool isActive = true;
+            const double PI = 3.14;
+            //3-	Define array with name car that hold more than 3 kinds on cars, then print all the cars with the array length.
+            string[] car = { "Toyota", "Honda", "Ford", "BMW" };
+            foreach (string item in car) { Console.WriteLine(item); }
+            Console.WriteLine($"the length of array is {car.Length}");
+            ////4-Write a program in C that reads a first name, surname and year of birth and display the names and the year one after another sequentially. Go to the editor
+
+            Console.Write("Input your firstname");
+            string Fname = Console.ReadLine();
+            Console.Write("Input your lastnamee");
+            string lname = Console.ReadLine();
+            Console.Write("Input your year of birthday");
+            string birthday = Console.ReadLine();
+            Console.WriteLine(Fname + lname + birthday);
+            //array
+            Console.WriteLine("Enter ten number");
+            int[] num = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"element  {i} :");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write($"Elements in array are:");
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write($"{num[i]} ");
+            }
+
+        }
+    }
+}
