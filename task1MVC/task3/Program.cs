997d6849640c07a7297f7167e3b3f2eb5bf24d60
duaameh <mehdawiduaa@gmail.com>
2024-07-14 13:19:41 +0300

    Add files via upload

---
+++ task1MVC/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car myCar = new Car(2023, "SUV", 35000, "Rav4", "Red" ,"1234");
+            //myCar.start();
+            //myCar.stop();
+            //Console.WriteLine(myCar.carInfo());
+            ///////////////////////////////
+            // task 7 (files):
+            //task7files fileobj = new task7files();
+            //string fileName = "my_info.txt";
+            //fileobj.WriteToFile(fileName);
+            //fileobj.ReadFromFile(fileName);
+            //fileobj.CountWords(fileName);
+            //fileobj.CountCharacters(fileName);
+            ///////////////////////////////////
+            /// task 3 (try catch)
+            //Trycatchtask obj = new Trycatchtask();
+            //obj.testTryAndCatch(12);
+            checkAge(15);
+        }
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+        }
+    }
+}
---
+++ task1MVC/task3/car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPcSharptask
+{
+    public class Car
+    {
+        int year;
+        string type;
+        double price;
+        string model;
+        string color;
+        string palletno;
+        public Car(int year, string type, double price, string model, string color, string palletno)
+        {
+            this.year = year;
+            this.type = type;
+            this.price = price;
+            this.model = model;
+            this.color = color;
+            this.palletno = palletno;
+        }
+        public void start()
+        {
+            Console.WriteLine("The car start.");
+        }
+        public void stop()
+        {
+            Console.WriteLine("The car stop.");
+        }
+        public string carInfo()
+        {
+            return "Car Year : " + year + "\nCar type : " + type + "\nCar price : "
+                   + price + "\nCar model : " + model +
+                   "\nCar color : " + color +
+                   "\npalletNo : " + palletno;
+        }
+
+    }
+}
